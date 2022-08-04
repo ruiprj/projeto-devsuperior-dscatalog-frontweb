2958975020f52ca7d40e1b96b4c1b0ed0d39b5df
Rui Palmeira Reis Junior <ruiprj@gmail.com>
2022-08-04 10:08:42 -0400

    07-11 React render e componente React

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
 import './assets/styles/custom.scss';
 import './App.css';
 
-function App() {
-  return <h1>Hello DSCatalog</h1>;
+// function App() {
+const App = () => {
+  return (
+    <>
+      <h1>Hello DSCatalog</h1>
+      <h1>Hello DSCatalog</h1>
+    </>
+  );
 }
 
 export default App;
