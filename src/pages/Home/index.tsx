35e57b66f6172fcb440eaeb72f6e2b27959ed0c7
Rui Palmeira Reis Junior <ruiprj@gmail.com>
2022-08-05 10:52:03 -0400

    07-21 Finalizando o layout X-Small

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,4 +1,5 @@
 import { ReactComponent as MainImage } from 'assets/images/main-image.svg';
+import ButttonIcon from 'components/ButtonIcon';
 import Navbar from 'components/Navbar';
 
 import './styles.css';
@@ -12,6 +13,9 @@ const Home = () => {
         <div className="home-card">
             <div className="home-content-container">
                 <h1>Conheça o melhor catálogo de produtos</h1>
+                <p>Ajudaremos você a encontrar os melhores produtos disponíveis no mercado</p>
+
+                <ButttonIcon />
             </div>
 
             <div className="home-image-container">
