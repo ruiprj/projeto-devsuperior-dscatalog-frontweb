852a8f946b26424e5edccf2a58a9688e46e17413
Rui Palmeira Reis Junior <ruiprj@gmail.com>
2022-08-09 11:04:51 -0400

    07-28 Rotas com React Router DOM

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import './assets/styles/custom.scss';
 import './App.css';
 
-import Catalog from 'pages/Catalog';
+import Routes from 'Routes';
 
 // const App = () => {
 function App() {
   return (
-    <Catalog />
+    <Routes />
   );
 }
 
--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,13 +1,15 @@
 import './styles.css';
 import 'bootstrap/js/src/collapse.js';
 
+import { Link, NavLink } from 'react-router-dom';
+
 const Navbar = () => {
   return (
     <nav className="navbar  navbar-expand-md  navbar-dark bg-primary  main-nav">
-      <div className="container-fluid">
-        <a href="link" className="nav-logo-text">
+      <div className="container-fluid"> {/** previne a quebra de linha entre logo e itens */}
+        <Link to="/" className="nav-logo-text">
           <h4>DS Catalog</h4>
-        </a>
+        </Link>
 
         <button
           className="navbar-toggler"
@@ -24,17 +26,15 @@ const Navbar = () => {
         <div className="collapse  navbar-collapse" id="dscatalog-navbar" >
           <ul className="navbar-nav  offset-md-2  main-menu">
             <li>
-              <a href="link" className="active">
-                HOME
-              </a>
+              <NavLink to="/" activeClassName="active" exact>HOME</NavLink>
             </li>
 
             <li>
-              <a href="link">CATÁLOGO</a>
+              <NavLink to="/products" activeClassName="active">CATÁLOGO</NavLink>
             </li>
 
             <li>
-              <a href="link">ADMIN</a>
+              <NavLink to="/admin" activeClassName="active">ADMIN</NavLink>
             </li>
           </ul>
         </div>
