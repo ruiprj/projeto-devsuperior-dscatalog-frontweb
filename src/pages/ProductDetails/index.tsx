3cebb8cac502580e1b5779483792fbf3512c99ad
Rui Palmeira Reis Junior <ruiprj@gmail.com>
2022-08-29 11:42:28 -0400

    08-27 Loaders para ProductDetails

--- src/pages/ProductDetails/index.tsx
+++ src/pages/ProductDetails/index.tsx
@@ -1,64 +1,81 @@
 import { ReactComponent as ArrowIcon } from 'assets/images/arrow.svg';
-import { Link, useParams } from 'react-router-dom';
-import ProductPrice from 'components/ProductPrice';
-import { Product } from 'types/product';
 import axios from 'axios';
+import ProductPrice from 'components/ProductPrice';
 import { useEffect, useState } from 'react';
+import { Link, useParams } from 'react-router-dom';
+import { Product } from 'types/product';
 
-import './styles.css';
 import { BASE_URL } from 'util/requests';
+import ProductDetailsLoader from './ProductDetailsLoader';
+import ProductInfoLoader from './ProductInfoLoader';
+import './styles.css';
 
 type UrlParams = {
-    productId: string;
-}
+  productId: string;
+};
 
 const ProductDetails = () => {
+  const { productId } = useParams<UrlParams>();
 
-    const { productId } = useParams<UrlParams>();
+  const [isLoading, setIsLoading] = useState(false);
 
-    const [product, setProduct] = useState<Product>();
-    
-    useEffect(() => {
+  const [product, setProduct] = useState<Product>();
 
-        axios.get(`${BASE_URL}/products/${productId}`)
-            .then(response => {
-                setProduct(response.data);
-            });
+  useEffect(() => {
+    setIsLoading(true);
 
-    }, [productId]); // o useEffect oberva o valor em colchetes; qdo ele é aterado, o useEffect é chamado novamente
-    
-    return (
-        <div className="product-details-container">
-            <div className="base-card  product-details-card">
-                <Link to="/products" >
-                    <div className="goback-container">
-                        <ArrowIcon />
-                        <h2>VOLTAR</h2>
-                    </div>
-                </Link>
-                
-                <div className="row">
-                    <div className="col-xl-6">
-                        <div className="img-container">
-                            <img src={ product?.imgUrl } alt={ product?.name } />
-                        </div>
+    axios
+      .get(`${BASE_URL}/products/${productId}`)
+      .then((response) => {
+        setProduct(response.data);
+      })
+      .finally(() => {
+        setIsLoading(false);
+      });
+  }, [productId]); // o useEffect oberva o valor em colchetes; qdo ele é aterado, o useEffect é chamado novamente
 
-                        <div className="name-price-container">
-                            <h1>{ product?.name }</h1>
-                            { product && <ProductPrice price={ product?.price } /> } {/* só será renderizado se o 'product' não for undefined */}
-                        </div>
-                    </div>
+  return (
+    <div className="product-details-container">
+      <div className="base-card  product-details-card">
+        <Link to="/products">
+          <div className="goback-container">
+            <ArrowIcon />
+            <h2>VOLTAR</h2>
+          </div>
+        </Link>
 
-                    <div className="col-xl-6">
-                        <div className="description-container">
-                            <h2>Descrição do produto</h2>
-                            <p>{ product?.description }</p>
-                        </div>
-                    </div>
+        <div className="row">
+          <div className="col-xl-6">
+            {isLoading ? (
+              <ProductInfoLoader />
+            ) : (
+              <>
+                <div className="img-container">
+                  <img src={product?.imgUrl} alt={product?.name} />
                 </div>
-            </div>
+
+                <div className="name-price-container">
+                  <h1>{product?.name}</h1>
+                  {product && <ProductPrice price={product?.price} />}  {/* só será renderizado se o 'product' não for undefined */}
+                </div>
+              </>
+            )}
+          </div>
+
+          <div className="col-xl-6">
+            {isLoading ? (
+              <ProductDetailsLoader />
+            ) : (
+              <div className="description-container">
+                <h2>Descrição do produto</h2>
+                <p>{product?.description}</p>
+              </div>
+            )}
+          </div>
         </div>
-    );
-}
+      </div>
+    </div>
+  );
+};
 
 export default ProductDetails;
