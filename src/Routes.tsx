865629ccae9c0ca62219f76c2f36827d0fc5befe
Rui Palmeira Reis Junior <ruiprj@gmail.com>
2022-09-05 11:29:53 -0400

    09-05 Sub-rotas da área auth

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -27,6 +27,7 @@ const Routes = () => {
                     <ProductDetails />
                 </Route>
 
+                <Redirect from="/admin/auth" to="/admin/auth/login" exact />
                 <Route path="/admin/auth">
                     <Auth />
                 </Route>
