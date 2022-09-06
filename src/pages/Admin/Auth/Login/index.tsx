7223979be677f4b4672871c59170077db49804ac
Rui Palmeira Reis Junior <ruiprj@gmail.com>
2022-09-06 11:55:42 -0400

    09-09 Estilizando o card de Login

--- src/pages/Admin/Auth/Login/index.tsx
+++ src/pages/Admin/Auth/Login/index.tsx
@@ -5,14 +5,14 @@ import './styles.css';
 
 const Login = () => {
   return (
-    <div className="base-card login-card">
+    <div className="base-card  login-card">
       <h1>LOGIN</h1>
-      
+
       <form>
         <div className="mb-4">
           <input
             type="text"
-            className="form-control base-input"
+            className="form-control  base-input"
             placeholder="Email"
             name="username"
           />
@@ -21,7 +21,7 @@ const Login = () => {
         <div className="mb-2">
           <input
             type="password"
-            className="form-control base-input "
+            className="form-control  base-input "
             placeholder="Password"
             name="password"
           />
