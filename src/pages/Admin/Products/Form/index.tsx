2da5df485d2253a73804ea3e18a41af11cac2127
Rui Palmeira Reis Junior <ruiprj@gmail.com>
2022-10-07 12:05:44 -0400

    10-10 Estilizando margens e containers do formulário

--- src/pages/Admin/Products/Form/index.tsx
+++ src/pages/Admin/Products/Form/index.tsx
@@ -7,13 +7,19 @@ const Form = () => {
         <h1 className="product-crud-form-title">DADOS DO PRODUTO</h1>
 
         <form action="">
-            <div className="row">
-                <div className="col-lg-6">
-                    <input type="text" className="form-control  base-input" />
+            <div className="row  product-crud-inputs-container">
+                <div className="col-lg-6  product-crud-inputs-left-container">
+                    <div className="margin-bottom-30">
+                        <input type="text" className="form-control  base-input" />
+                    </div>
 
-                    <input type="text" className="form-control  base-input" />
+                    <div className="margin-bottom-30">
+                        <input type="text" className="form-control  base-input" />
+                    </div>
 
-                    <input type="text" className="form-control  base-input" />
+                    <div>
+                        <input type="text" className="form-control  base-input" />
+                    </div>
                 </div>
 
                 <div className="col-lg-6">
@@ -22,7 +28,7 @@ const Form = () => {
                 </div>
             </div>
 
-            <div>
+            <div className="product-crud-buttons-container">
                 <button className="btn  btn-outline-danger">CANCELAR</button>
                 <button className="btn  btn-primary">SALVAR</button>
             </div>
