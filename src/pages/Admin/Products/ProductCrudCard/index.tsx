a25098889cfb8b6d936b864492b3edf73944d7a6
Rui Palmeira Reis Junior <ruiprj@gmail.com>
2023-01-18 10:59:16 -0400

    10-18 Função que deleta um produto

--- src/pages/Admin/Products/ProductCrudCard/index.tsx
+++ src/pages/Admin/Products/ProductCrudCard/index.tsx
@@ -4,47 +4,66 @@ import ProductPrice from 'components/ProductPrice';
 import { Product } from 'types/product';
 import CategoryBadge from '../CategoryBadge';
 import { Link } from 'react-router-dom';
+import { AxiosRequestConfig } from 'axios';
+import { requestBackend } from 'util/requests';
 
 type Props = {
-    product: Product;
-}
+  product: Product;
+};
 
-const ProductCrudCard = ({ product } : Props) => {
-    return (
-        <div className="base-card  product-crud-card">
-            <div className="product-crud-card-top-container">
-                <img src={ product.imgUrl } alt={ product.name } />
-            </div>
+const ProductCrudCard = ({ product }: Props) => {
+  const handleDelete = (productId: number) => {
 
-            <div className="product-crud-card-description">
-                <div className="product-crud-card-bottom-container">
-                    <h6>{ product.name }</h6>
-                    <ProductPrice price={ product.price } />
-                </div>
+    if (!window.confirm("Tem certeza que deseja deletar?")) {
+        return;
+    }
 
-                <div className="product-crud-categories-container">
-                    {
-                        product.categories.map((category) => (
-                            <CategoryBadge name={ category.name } key={ category.id } />
-                        ))
-                    }
-                </div>
-            </div>
+    const config: AxiosRequestConfig = {
+      method: 'DELETE',
+      url: `/products/${productId}`,
+      withCredentials: true,
+    };
 
-            <div className="product-crud-card-buttons-container">
-                <button className="btn  btn-outline-danger  product-crud-card-button  product-crud-card-button-first">
-                    EXCLUIR
-                </button>
+    requestBackend(config).then(() => {
+      console.log('DELETADO ID ' + productId);
+    });
 
-                <Link to={`/admin/products/${product.id}`}>
-                    <button className="btn  btn-outline-secondary  product-crud-card-button">
-                        EDITAR
-                    </button>
-                </Link>
-            </div>
+  };
+
+  return (
+    <div className="base-card  product-crud-card">
+      <div className="product-crud-card-top-container">
+        <img src={product.imgUrl} alt={product.name} />
+      </div>
+
+      <div className="product-crud-card-description">
+        <div className="product-crud-card-bottom-container">
+          <h6>{product.name}</h6>
+          <ProductPrice price={product.price} />
         </div>
-    );
-}
+
+        <div className="product-crud-categories-container">
+          {product.categories.map((category) => (
+            <CategoryBadge name={category.name} key={category.id} />
+          ))}
+        </div>
+      </div>
+
+      <div className="product-crud-card-buttons-container">
+        <button
+          onClick={() => handleDelete(product.id)} 
+          className="btn  btn-outline-danger  product-crud-card-button  product-crud-card-button-first">
+            EXCLUIR
+        </button>
+
+        <Link to={`/admin/products/${product.id}`}>
+          <button className="btn  btn-outline-secondary  product-crud-card-button">
+            EDITAR
+          </button>
+        </Link>
+      </div>
+    </div>
+  );
+};
 
 export default ProductCrudCard;
-
