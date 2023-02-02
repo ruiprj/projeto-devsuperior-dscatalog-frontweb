e9cde483a146ceedb780d3a6f7665453ebb937f6
Rui Palmeira Reis Junior <ruiprj@gmail.com>
2023-02-02 11:00:54 -0400

    10-33 HTML do ProductFilter

--- src/components/ProductFilter/index.tsx
+++ src/components/ProductFilter/index.tsx
@@ -4,7 +4,28 @@ import './styles.css';
 const ProductFilter = () => {
 
     return (
-        <div className="base-card  product-filter-container">Search bar</div>
+        <div className="base-card  product-filter-container">
+            
+            <form action="" className="product-filter-form">
+                <div className="product-filter-name-container">
+                    <input 
+                        type="text" 
+                        className="form-control"
+                    />
+                </div>
+
+                <div className="product-filter-bottom-container">
+                    <div className="product-filter-category-container">
+                        <select name="" id="">
+                            <option value="">Livros</option>
+                        </select>
+                    </div>
+
+                    <button className="btn  btn-outline-secondary">LIMPAR</button>
+                </div>
+            </form>
+            
+        </div>
     );
 
 }
