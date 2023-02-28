537e0edbb7e4875c8dfef9327a7b3567398acc1c
Rui Palmeira Reis Junior <ruiprj@gmail.com>
2023-02-28 10:54:28 -0400

    11-32 Mock do React Router DOM e useParams

--- src/pages/Admin/Products/__tests__/Form.spec.tsx
+++ src/pages/Admin/Products/__tests__/Form.spec.tsx
@@ -1,15 +1,29 @@
 import { render, screen } from '@testing-library/react';
 import Form from '../Form';
 import history from 'util/history';
-import { Router } from 'react-router-dom';
+import { Router, useParams } from 'react-router-dom';
 
-test('should render Form', () => {
-  render(
-    // o Router no teste é obrigatório pois existe um Switch dentro do Form -> o Switch deve estar dentro de um Router durante um teste
-    <Router history={history}>
-      <Form />
-    </Router>
-  );
+jest.mock('react-router-dom', () => ({
+  ...jest.requireActual('react-router-dom'),
+  useParams: jest.fn(),
+}));
 
-  screen.debug();
+describe('Product form create tests', () => {
+
+  beforeEach(() => {
+    (useParams as jest.Mock).mockReturnValue({
+        productId: 'create'
+    })
+  });
+
+  test('should render Form', () => {
+    render(
+      // o Router no teste é obrigatório pois existe um Switch dentro do Form -> o Switch deve estar dentro de um Router durante um teste
+      <Router history={history}>
+        <Form />
+      </Router>
+    );
+
+    screen.debug();
+  });
 });
