485e2671b145af429e770272ff0dd2a4e3b082c1
Rui Palmeira Reis Junior <ruiprj@gmail.com>
2023-02-28 10:29:58 -0400

    11-29 Mock da API com MSW, trabalhando com fixtures

--- src/pages/Catalog/__tests__/Catalog.spec.tsx
+++ src/pages/Catalog/__tests__/Catalog.spec.tsx
@@ -1,24 +1,28 @@
-import { render, screen, waitFor } from "@testing-library/react";
-import Catalog from "..";
-import history from "util/history";
-import { Router } from "react-router-dom";
+import { render, screen, waitFor } from '@testing-library/react';
+import Catalog from '..';
+import history from 'util/history';
+import { Router } from 'react-router-dom';
+import { server } from './fixtures';
+
+beforeAll(() => server.listen());
+afterEach(() => server.resetHandlers());
+afterAll(() => server.close());
 
 test('should render Catalog with products', async () => {
-
     render(
-        <Router history={ history }>
-            <Catalog />
-        </Router>
+    <Router history={history}>
+        <Catalog />
+    </Router>
     );
 
     // screen.debug();
 
-    expect(screen.getByText("Catálogo de produtos")).toBeInTheDocument();
+    expect(screen.getByText('Catálogo de produtos')).toBeInTheDocument();
 
     await waitFor(() => {
-        expect(screen.getByText("Smart TV")).toBeInTheDocument();
+    expect(screen.getByText('Smart TV')).toBeInTheDocument();
     });
 
     // screen.debug();
-
 });
+
