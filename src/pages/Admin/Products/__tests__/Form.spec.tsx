ea402c351f3dd3113895b303adbfab5658b676f7
Rui Palmeira Reis Junior <ruiprj@gmail.com>
2023-03-02 10:30:45 -0400

    11-36 Simulando a digitação nas caixas de texto com userEvent

--- src/pages/Admin/Products/__tests__/Form.spec.tsx
+++ src/pages/Admin/Products/__tests__/Form.spec.tsx
@@ -2,6 +2,7 @@ import { render, screen } from '@testing-library/react';
 import Form from '../Form';
 import history from 'util/history';
 import { Router, useParams } from 'react-router-dom';
+import userEvent from '@testing-library/user-event';
 
 jest.mock('react-router-dom', () => ({
   ...jest.requireActual('react-router-dom'),
@@ -29,5 +30,11 @@ describe('Product form create tests', () => {
     const imgUrlInput = screen.getByTestId("imgUrl");
     const descriptionInput = screen.getByTestId("description");
     const categoriesInput = screen.getByLabelText("Categorias");
+
+    userEvent.type(nameInput, 'Computador');
+    userEvent.type(priceInput, '5000.12');
+    userEvent.type(imgUrlInput, 'https://raw.githubusercontent.com/devsuperior/dscatalog-resources/master/backend/img/1-big.jpg');
+    userEvent.type(descriptionInput, 'Computador legalzão');
+    // userEvent.type(categoriesInput, 'Computador');
   });
 });
